4e05991dbe2b40008a90b981fa6931d8b147b6d9
Harsh Raundal <dkingston0000@gmail.com>
2026-08-20 14:09:13 +0530

    Create Math Functions.cpp

---
+++ Math Functions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+    cout<<pow(6,3)<<endl;
+    cout<<round(5.8)<<endl;
+    cout<<fmax(4,6)<<endl;
+    cout<<ceil(6.1)<<endl;
+    cout<<floor(6.1)<<endl;
+    cout<<sqrt(25);
+    
+    return 0;
+}

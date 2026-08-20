2cbb18e44a0120c5ad108466eee4ab7ff6778836
Harsh Raundal <dkingston0000@gmail.com>
2026-08-20 12:45:25 +0530

    Add First.cpp with agent details

---
+++ First.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string CharName = "Holmes";
+    int CharAge = 20;
+    std::cout<<"The agent " <<CharName<<" is going to die tomorrow!"<<endl;
+    std::cout<<"The agent age is "<<CharAge<<endl;
+    std::cout<<"The agent name is "<<CharName<<endl;
+}

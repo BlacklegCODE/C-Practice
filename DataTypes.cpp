7c9ac51c9632708a1852d4bded4def6e7f010bc4
Harsh Raundal <dkingston0000@gmail.com>
2026-08-20 13:06:07 +0530

    Create DataTypes.cpp

---
+++ DataTypes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Harsh";
+    int age = 21;
+    float pi = 3.14;
+    double gpa = 7.66;
+    bool isPresent = true;
+    char Grade = 'A';
+
+    cout<<name<< " is of age " << age << " who calculated "<<endl;
+    cout <<pi<<" and got a gpa of "<< gpa<<" with final grade "<<Grade<<endl;
+    cout<<" Is he here? "<<isPresent;
+    return 0;
+}

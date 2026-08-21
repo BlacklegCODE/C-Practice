c3bd32ae847f3dc7a301e937ee970060a80295d7
Harsh Raundal <dkingston0000@gmail.com>
2026-08-21 15:39:35 +0530

    Create MostBasic Calculator.cpp

---
+++ MostBasic Calculator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,ans;
+    cout<<"Enter a :";
+    cin>>a;
+
+    cout<<"Enter b :";
+    cin>>b;
+    ans = a+b;
+    cout<<"Answer :"<<ans;
+
+    return 0;
+}

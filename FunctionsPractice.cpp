adb5d6a313c47dc0388e6ce20a9717ac98019d00
Harsh Raundal <dkingston0000@gmail.com>
2026-08-23 17:40:02 +0530

    Create FunctionsPractice.cpp

---
+++ FunctionsPractice.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+void printer(string name, int age)
+{
+    cout<<"Your name is :"<<name<<endl;
+    cout<<"Your age is :"<<age;
+}
+int main()
+{
+    printer("Harsh",55);
+    return 0;
+}
